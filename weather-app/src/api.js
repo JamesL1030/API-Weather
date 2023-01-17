4498dc578b1ae6de1afa093bb152ed998b26c7b8
James Lucas <jameslucas@Jamess-MBP.attlocal.net>
2023-01-17 16:13:22 -0500

    added search component

---
+++ weather-app/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+       'X-RapidAPI-Key': '59ac6c80f5msh970f98079c44a08p1fde2cjsn7699de4115b4',
+       'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
