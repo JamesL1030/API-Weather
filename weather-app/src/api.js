6d54761f0116799cf30def785f1991b09d72c2ab
James Lucas <jameslucas@Jamess-MBP.attlocal.net>
2023-01-21 14:42:10 -0500

    added current component

--- weather-app/src/api.js
+++ weather-app/src/api.js
@@ -7,3 +7,6 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "108bed69d959699fe0a38d8164dbc3bb";
